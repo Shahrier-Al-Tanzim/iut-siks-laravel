36f341852b9678b925e3ce7ff59f040af681aef0
Ryuzaki-L-07 <shahrier@iut-dhaka.edu>
2025-06-28 22:05:45 +0600

    Initial commit
    Project files added to set up the initial codebase.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return 'Test route works!';
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::get('/hello', [HelloController::class, 'show']);
 Route::get('/contact', [ContactController::class, 'create']);
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+require __DIR__.'/auth.php';
